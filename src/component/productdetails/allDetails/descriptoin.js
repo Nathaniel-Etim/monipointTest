b6ca55182c215eceb50c2c59c0b0daaf79019281
Nathaniel <nathjoeetim@gmail.com>
2023-04-03 13:03:48 +0100

    fixed bug

--- src/component/productdetails/allDetails/descriptoin.js
+++ src/component/productdetails/allDetails/descriptoin.js
@@ -1,20 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./decription.css";
-import { BsArrowUp } from "react-icons/bs";
+import { BsArrowUp, BsArrowDown } from "react-icons/bs";
 
 function Description() {
+  const [increaseHeight, setHeight] = useState(false);
+
+  function OnReducePheightfn() {
+    setHeight((previous) => !previous);
+  }
+
   return (
     <div className="description">
       <div className="description-header">
         <span className="header-text">Description</span>
       </div>
-      <p>
+      <p
+        style={{
+          height: `${increaseHeight ? "fit-content" : "1rem"}`,
+          overflow: "hidden",
+        }}
+      >
         .long sleeve <br /> .Easy to wash <br /> .does not remove color
         <br /> Chat us to know more about the product.
       </p>
-      <span className="more">
-        See less
-        <BsArrowUp className="arrowup" />
+      <span className="more" onClick={OnReducePheightfn}>
+        See {increaseHeight ? "less" : "more"}
+        {increaseHeight ? (
+          <BsArrowUp className="arrowup" />
+        ) : (
+          <BsArrowDown className="arrowup" />
+        )}
       </span>
     </div>
   );
